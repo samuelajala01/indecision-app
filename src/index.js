88eeff67ffcb451e922cce091a58ef633f61a6bf
Samuel Ajala <samuelajala01@gmail.com>
2022-04-03 22:00:22 +0100

    build file created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import IndecisionApp from "./components/IndecisionApp"
-import "normalize.css/normalize.css"
 import "../src/index.css"
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("root"))
